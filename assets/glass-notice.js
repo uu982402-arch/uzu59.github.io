3e52cbfa88e33e5d1088c0567861f9597fc307b4
uu982402-arch <uu982402@gmail.com>
2026-02-15 00:40:37 +0900

    Add files via upload

--- assets/glass-notice.js
+++ assets/glass-notice.js
@@ -1,16 +1,1 @@
-/* Optional notice bar behavior */
-(function(){
-  try{
-    var n=document.querySelector('.glass-notice');
-    if(!n) return;
-    var key='88_glass_notice_dismissed';
-    if(localStorage.getItem(key)==='1'){ n.remove(); return; }
-    var btn=n.querySelector('[data-dismiss]');
-    if(btn){
-      btn.addEventListener('click', function(){
-        try{ localStorage.setItem(key,'1'); }catch(e){}
-        n.remove();
-      });
-    }
-  }catch(e){}
-})();
+!function(){try{var e=document.querySelector(".glass-notice");if(!e)return;var t="88_glass_notice_dismissed";if("1"===localStorage.getItem(t))return void e.remove();var r=e.querySelector("[data-dismiss]");r&&r.addEventListener("click",function(){try{localStorage.setItem(t,"1")}catch(e){}e.remove()})}catch(e){}}();
